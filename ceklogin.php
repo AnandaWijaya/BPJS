ab576d4f6d31237aef8d828659aa988e67a7c919
AnandaWijaya <nanda.w46@gmail.com>
2021-05-08 18:45:31 +0700

    commit pertama

---
+++ ceklogin.php
@@ -0,0 +1,38 @@
+<?php
+
+session_start();
+
+include 'koneksi.php';
+
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+
+$login = mysqli_query($koneksi,"SELECT * FROM login WHERE username='$username' and password='$password'");
+$cek = mysqli_num_rows($login);
+
+if($cek > 0){
+
+ $data = mysqli_fetch_assoc($login);
+
+ if($data['level']=="admin"){
+
+  $_SESSION['username'] = $username;
+  $_SESSION['level'] = "admin";
+  header("location:admin/index.php");
+
+ }else if($data['level']=="enggineering"){
+  $_SESSION['username'] = $username;
+  $_SESSION['level'] = "enggineering";
+  header("location:index_pimpinan.php");
+
+ }else{
+
+  header("location:index.php");
+ } 
+}else{
+ header("location:index.php");
+}
+
+?>
